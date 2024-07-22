b5dfb39279fab0849253967451d019a26cfdbca7
cmduc008 <cmduc008@gmail.com>
2024-07-22 14:32:39 +0700

    update

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import * as img1 from "./image/book2.jpg"
+import * as img2 from "./image/book2.jpg"
+import * as img3 from "./image/book2.jpg"
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: img1,
+    id: 1, 
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Stephen King',
+    title: 'Fairy Tale',
+    img: img3,
+    id: 3,
+  },
+];
